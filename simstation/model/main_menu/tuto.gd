dc8a470c08202cac5d3cdc65c64ea3a5b11fbd4d
PanDox42 <131681753+PanDox42@users.noreply.github.com>
2025-12-19 00:55:43 +0100

    Correction des fautes d'orthographe du tuto

--- simstation/model/main_menu/tuto.gd
+++ simstation/model/main_menu/tuto.gd
@@ -2,17 +2,17 @@ extends CanvasLayer
 
 # Liste des données : [Chemin Image, Texte de description]
 var tuto_data = [
-	["res://assets/tuto/CaptureMainScreen.PNG", "Bienvenue sur SimStation ! Vous allez devoit gérer votre base en antarctique."],
-	["res://assets/tuto/CapturePlayScreen.PNG", "Voici votre zone de construction. Ici vous pouvez ajouter des batiments en les faisant glisser sur la droite"],
+	["res://assets/tuto/CaptureMainScreen.PNG", "Bienvenue sur SimStation ! Vous allez devoir gérer votre base en Antarctique."],
+	["res://assets/tuto/CapturePlayScreen.PNG", "Voici votre zone de construction. Ici, vous pouvez ajouter des bâtiments en les faisant glisser depuis la gauche."],
 	["res://assets/tuto/CaptureStats.PNG", "Analysez vos graphiques pour anticiper les crises de santé ou de moral."],
-	["res://assets/tuto/CaptureStatsDescend.PNG", "Vos stats descendent trop vite ?\nAjoutez une chaufferie pour chauffer vos batiments !"],
-	["res://assets/tuto/CaptureStatsStable.PNG", "Et voilà ! Avec 2 chaufferie, la station arrive amplement à subvenir à ces besoins en chaleur !"],
-	["res://assets/tuto/CaptureStatsHopital.PNG", "Mais vos habitants doivent aussi se soigner ! Construisez un hopital et il augmentera votre santé globale !"],
-	["res://assets/tuto/CaptureCatastrophe.PNG", "Mais attention, il se peux que vous rencontriez des catastrophe naturelles qui ferons descendre vos stats"],
-	["res://assets/tuto/CaptureDescription.PNG", "Quand vous cliquer sur un batiment, vous verrez sa description, sa santé, et le bonheur qu'il confère aux habitants."],
-	["res://assets/tuto/CaptureRechercheBouton.PNG", "Pour accéder à l'arbre de recherche, il faut placer un laboratoire et cliquer sur le boutons Recherche qui est dans sa description"],
-	["res://assets/tuto/CaptureRechercheScreen.PNG", "L'arbre de recherche vous permet de débloquer des technologies, des batiments et ce sera votre unique source de revenu"],
-	["res://assets/tuto/CaptureShop.PNG", "La boutique vous permet d'acheter de nouveaux bâtiments (sous conditions qu'ils ont été débloqués avec les recherches)."],
+	["res://assets/tuto/CaptureStatsDescend.PNG", "Vos stats descendent trop vite ?\nAjoutez une chaufferie pour chauffer vos bâtiments !"],
+	["res://assets/tuto/CaptureStatsStable.PNG", "Et voilà ! Avec deux chaufferies, la station arrive amplement à subvenir à ses besoins en chaleur !"],
+	["res://assets/tuto/CaptureStatsHopital.PNG", "Mais vos habitants doivent aussi se soigner ! Construisez un hôpital et il augmentera votre santé globale !"],
+	["res://assets/tuto/CaptureCatastrophe.PNG", "Mais attention, il se peut que vous rencontriez des catastrophes naturelles qui feront descendre vos stats."],
+	["res://assets/tuto/CaptureDescription.PNG", "Quand vous cliquez sur un bâtiment, vous verrez sa description, sa santé, et le bonheur qu'il confère aux habitants."],
+	["res://assets/tuto/CaptureRechercheBouton.PNG", "Pour accéder à l'arbre de recherche, il faut placer un laboratoire et cliquer sur le bouton Recherche qui est dans sa description."],
+	["res://assets/tuto/CaptureRechercheScreen.PNG", "L'arbre de recherche vous permet de débloquer des technologies et des bâtiments ; ce sera votre unique source de revenus."],
+	["res://assets/tuto/CaptureShop.PNG", "La boutique vous permet d'acheter de nouveaux bâtiments (à condition qu'ils aient été débloqués par les recherches)."],
 ]
 
 var index_actuel = 0
